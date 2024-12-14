04f466625530ee1c4bdb8e7638c967addbab13b2
Sunil PVS <sunil.pvs@pvs-consultancy.com>
2024-12-14 08:11:04 +0530

    modified:   app.ini 	new file:   app_pvs.ini 	modified:   email/Email.php

--- email/Email.php
+++ email/Email.php
@@ -8,8 +8,53 @@
 
     class Email
     {
+        private $email_host;
+        private $email_port;
+        private $email_name;
+        private $smtp_auth;
+        private $email_user;
+        private $app_pwd;
+        private $url_home;
+        private $url_about;
+        private $url_buss;
+        private $url_contact;
+        private $app_contact;
+        //Address Variables
+        private $app_hadd1;
+        private $app_hadd2;
+        private $app_hadd3;
+        private $app_hadd4;
+        private $app_cadd1;
+        private $app_cadd2;
+        private $app_cadd3;
+        private $app_cadd4;
+
+
         function __construct() 
         {
+            $ini_file_path = $_SERVER['DOCUMENT_ROOT'] ."/app.ini";
+            $ini_file = parse_ini_file($ini_file_path);
+
+            $this->email_host = $ini_file["email_host"];
+            $this->email_port = $ini_file["email_port"];
+            $this->email_name = $ini_file["email_name"];
+            $this->smtp_auth = $ini_file["smtp_auth"];
+            $this->email_user = $ini_file["email_user"];
+            $this->app_pwd = $ini_file["app_pwd"];
+            $this->url_home = $ini_file["url_home"];
+            $this->url_about = $ini_file["url_about"];
+            $this->url_buss = $ini_file["url_buss"];
+            $this->url_contact = $ini_file["url_contact"];
+            $this->app_contact = $ini_file["app_contact"];
+            //
+            $this->app_hadd1 = $ini_file["app_hadd1"];
+            $this->app_hadd2 = $ini_file["app_hadd2"];
+            $this->app_hadd3 = $ini_file["app_hadd3"];
+            $this->app_hadd4 = $ini_file["app_hadd4"];
+            $this->app_cadd1 = $ini_file["app_cadd1"];
+            $this->app_cadd2 = $ini_file["app_cadd2"];
+            $this->app_cadd3 = $ini_file["app_cadd3"];
+            $this->app_cadd4 = $ini_file["app_cadd4"];
         }
 
         function sendEmailNotification($subject,$toAddress,$greetings, $salutation, $message)
@@ -19,17 +64,17 @@
             try 
             {
                 //Server settings
-                //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-                $mail->isSMTP();                                            //Send using SMTP
-                $mail->Host       = 'smtp.office365.com';                     //Set the SMTP server to send through
-                $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                $mail->Username   = 'donotreply@shrichandragroup.com';                     //SMTP username
-                $mail->Password   = 'xxzmgnvhyfqxwpgw';                               //SMTP password
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption For SSL : ENCRYPTION_SMTPS
-                $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+                //$mail->SMTPDebug = SMTP::DEBUG_SERVER;             //Enable verbose debug output
+                $mail->isSMTP();                                     //Send using SMTP
+                $mail->Host       = $this->email_host;               //Set the SMTP server to send through
+                $mail->SMTPAuth   = $this->smtp_auth;                //Enable SMTP authentication
+                $mail->Username   = $this->email_user;               //SMTP username
+                $mail->Password   = $this->app_pwd;;                 //SMTP password
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;  //Enable implicit TLS encryption For SSL : ENCRYPTION_SMTPS
+                $mail->Port       = $this->email_port;               //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
                 //Recipients
-                $mail->setFrom('donotreply@shrichandragroup.com', 'ShriChandraGroup');
+                $mail->setFrom($this->email_user, $this->email_name);
                 $mail->AddAddress($toAddress);
                 //$mail->addAddress('sunil_pvs@hotmail.com', 'Sunil PVS');     //Add a recipient
                 //$mail->addAddress('ellen@example.com');               //Name is optional
@@ -50,15 +95,6 @@
                 $mail->send();
                 return true;
                 //echo 'Message has been sent';
-          		//if(!$mail->Send())
-                //{
-//			        echo $mail->ErrorInfo;
-			        //return false;
-		        //}else
-                //{
-		            //	echo 'Sent';
-			      //  return true;
-		        //}
             } catch (Exception $e) 
             {
                 //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
@@ -68,6 +104,7 @@
 
         function createBody($greetings, $salutation, $message)
         {
+
             $body = '<!DOCTYPE html>
                     <html>
                     <head>
@@ -97,23 +134,22 @@
                             <td>
                             </td>
                             <td><img src="cid:logo">
-                            <h3>Call us / WhatsApp<br>0884-2361567,69</h3>
+                            <h3>Call us / WhatsApp<br>'.$this->app_contact.'</h3>
                             </td>
                             <td>
                                 <h3>Head Office:</h3>
-                                <p> 7-70-62/A, 1st Floor, <br>Ramya Royale, Ramanayya Peat,<br> Kakinada-533 003, A.P. INDIA,<br> GST: 37AAECR9430L1ZX.</p>
+                                <p>'.$this->app_hadd1.'<br>'.$this->app_hadd2.'<br>'.$this->app_hadd3.'<br>'.$this->app_hadd4.'</p>
                             </td>
                             <td>
                                 <h3>Corporate Office:</h3>
-                                <p>Flat No: 904, 910, <br>Sagar Tech Plaza, B-Andheri Kurla Sakinaka,<br> Andheri East, Mumbai-400072,<br> Maharasta India.<br> GST: 27AAECR9430L1ZY. </p><br><br>
+                                <p>'.$this->app_cadd1.'<br>'.$this->app_cadd2.'<br>'.$this->app_cadd3.'<br>'.$this->app_cadd4.'</p><br><br>
                             </td>
                             <td>
-                            <h3>About Us</h3>
-                            <p><a href="https://shrichandragroup.com/" style="text-decoration: none;">Home</a></p>
-                            <p><a href="https://shrichandragroup.com/" style="text-decoration: none;">About</a></p>
-                            <p><a href="https://shrichandragroup.com/" style="text-decoration: none;">Our Business</a></p>
-                            <p><a href="https://shrichandragroup.com/" style="text-decoration: none;">Team</a></p>
-                            <p><a href="https://shrichandragroup.com/" style="text-decoration: none;">Contact Us</a></p>
+                            <h3>About Us</h3> 
+                            <p><a href="'.$this->url_home.'" style="text-decoration: none;">Home</a></p>
+                            <p><a href="'.$this->url_about.'" style="text-decoration: none;">About</a></p>
+                            <p><a href="'.$this->url_buss.'" style="text-decoration: none;">Our Business</a></p>
+                            <p><a href="'.$this->url_contact.'" style="text-decoration: none;">Contact Us</a></p>
                             </td>
                             <td>
                             </td>
@@ -122,7 +158,7 @@
                 </table>
                 <center> <table>
                 <tr>
-                    <td>© 2024 Shrichandra Group. All Right Reserved.</td>
+                    <td>©2024 Shrichandra Group. All Right Reserved.</td>
                 </tr>
                 </table></center>
                 </body>
