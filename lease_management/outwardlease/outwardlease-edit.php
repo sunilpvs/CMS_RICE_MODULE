efe9ebb539ac7a404cd635309cea3c40ab57ac03
Sunil PVS <sunil.pvs@pvs-consultancy.com>
2025-03-11 12:51:18 +0530

    modified:   lease_management/outwardlease/outwardlease-edit.php

--- lease_management/outwardlease/outwardlease-edit.php
+++ lease_management/outwardlease/outwardlease-edit.php
@@ -4,7 +4,7 @@
   require_once($_SERVER['DOCUMENT_ROOT'] .'/lease_management/outwardlease/Outwardlease.php');
   include($_SERVER['DOCUMENT_ROOT'] .'/includes/header.php'); 
   include($_SERVER['DOCUMENT_ROOT'] .'/includes/navbar.php');
-  include($_SERVER['DOCUMENT_ROOT'] .'/includes/Generic.php');
+  require_once($_SERVER['DOCUMENT_ROOT'] .'/includes/Generic.php');
 
   if (!empty($result)){
       $row1 = mysqli_fetch_array($result, MYSQLI_ASSOC);
