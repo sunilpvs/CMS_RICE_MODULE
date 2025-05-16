55986d27b255c7509a0072d9a4a8dbbf553f4699
Sunil PVS <sunil.pvs@pvs-consultancy.com>
2025-05-16 07:53:44 +0530

    modified:   .htaccess 	modified:   app.ini 	new file:   app_demo.ini 	deleted:    app_pvs.ini 	modified:   index.php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-    <title>Bootstrap Example</title>
+    <title>CMS</title>
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
     
 
